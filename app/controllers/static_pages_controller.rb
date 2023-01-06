e0ffb0bf0bcac63f6fd8cb493f7e6a1ab0fbd61b
valdenio458 <valdeniomartinho@gmail.com>
2023-01-06 14:25:57 -0300

    Criando Model Contact

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,7 +7,7 @@ class StaticPagesController < ApplicationController
   def about
   end 
 
-  def contact
+  def contacts
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :contacts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
 
   get 'about', to: 'static_pages#about'
 
-  get 'contact', to: 'static_pages#contact'
+  get 'contacts', to: 'static_pages#contact'
 
   
 end
