ace4a71fbbfce1f78410eb21368caf7171891cd8
valdenio458 <valdeniomartinho@gmail.com>
2023-01-08 11:22:20 -0300

    Controllers e Validações - Parte 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  resources :contacts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "static_pages#index"
-
+  
   get 'about', to: 'static_pages#about'
-
+  
   get 'contacts', to: 'static_pages#contact'
-
+  
+  resources :contacts
+  resource  :users, only: [:new, :create]
   
 end
