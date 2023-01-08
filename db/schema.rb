57f10030d3a0e769ae2aa217765e7a0ad4c3d6c4
valdenio458 <valdeniomartinho@gmail.com>
2023-01-08 10:40:41 -0300

    Criando Model User

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_06_165416) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_08_132539) do
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", default: "", null: false
+    t.string "password_digest", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
